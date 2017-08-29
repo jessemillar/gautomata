bc03301afc4bbf88938712280bad9d597df5fa6e
Jesse Millar <hellojessemillar@gmail.com>
2017-08-29 10:50:57 -0500

    Working

--- server.go
+++ server.go
@@ -7,32 +7,56 @@ import (
 	"image/color"
 	"image/draw"
 	"image/png"
+	"math/rand"
 	"os"
+	"time"
 )
 
 var (
-	white color.Color = color.RGBA{255, 255, 255, 255}
-	black color.Color = color.RGBA{0, 0, 0, 255}
-	blue  color.Color = color.RGBA{0, 0, 255, 255}
+	dark  color.Color = color.RGBA{68, 71, 90, 255}
+	light color.Color = color.RGBA{98, 114, 164, 255}
 )
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
 	// Get params
-	w := *flag.Int("w", 256, "the width of the resulting image")
-	h := *flag.Int("h", 256, "the height of the resulting image")
-	output := *flag.String("o", "automata.png", "the filename of the resulting image")
+	w := flag.Int("w", 256, "the width of the resulting image")
+	h := flag.Int("h", 256, "the height of the resulting image")
+	output := flag.String("o", "automata.png", "the filename of the resulting image")
 	flag.Parse()
 
-	m := image.NewRGBA(image.Rect(0, 0, w, h))
-	draw.Draw(m, m.Bounds(), &image.Uniform{blue}, image.ZP, draw.Src) // Fill with a uniform color
+	m := image.NewRGBA(image.Rect(0, 0, *w, *h))
+	draw.Draw(m, m.Bounds(), &image.Uniform{dark}, image.ZP, draw.Src) // Fill with a uniform color
 
-	// Drawing happens here
-	m.Set(10, 10, white)
-	fmt.Println(m.At(0, 0))
-	fmt.Println(m.At(10, 10))
+	// Random initial state
+	for i := 0; i < *w; i++ {
+		if rand.Intn(2) < 1 {
+			m.Set(i, 0, light)
+		}
+	}
+
+	// Loop through the y axis
+	for y := 1; y < *h; y++ {
+		for x := 0; x < *w; x++ {
+			draw := 0
+
+			if m.At(x-1, y-1) == dark {
+				draw++
+			}
+
+			if m.At(x+1, y-1) == dark {
+				draw++
+			}
+
+			if draw == 1 {
+				m.Set(x, y, light)
+			}
+		}
+	}
 
 	// Make the file
-	f, err := os.OpenFile(output, os.O_WRONLY|os.O_CREATE, 0600)
+	f, err := os.OpenFile(*output, os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -42,26 +66,3 @@ func main() {
 
 	png.Encode(f, m)
 }
-
-/*
-for i=0,127 do pset(i,0,flr(rnd(2))) end
---random initial states ^^^
-palt(0,false)
-
-function _draw()
- memcpy(0x0,0x6000,0x2000)
- spr(0,0,1,16,16)
- line(0,0,127,0,0)
-
- for x=0,127 do
-  local k=0
-
-  k+=pget(x-1,1)
-  k+=pget(x+1,1)
-
-  if k==1 then
-   pset(x,0,(k)%2)
-  end
- end
-end
-*/
