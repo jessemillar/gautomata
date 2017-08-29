0f4fd6a3abd856e90c00330cc2324ef7c6fef4ef
Jesse Millar <hellojessemillar@gmail.com>
2017-08-29 14:42:11 -0500

    Added hex string parsing

--- server.go
+++ server.go
@@ -9,13 +9,13 @@ import (
 	"image/png"
 	"math/rand"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/jessemillar/gautomata/cells"
 	"github.com/jessemillar/gautomata/tools"
 )
-
-var mix color.Color = color.RGBA{255, 255, 255, 255}
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
@@ -23,12 +23,23 @@ func main() {
 	// Get params
 	w := flag.Int("w", 256, "the width of the resulting image")
 	h := flag.Int("h", 256, "the height of the resulting image")
+	col := flag.String("c", "#ffffff", "the color to use as an average for randomly-generated colors")
 	output := flag.String("o", "automata.png", "the filename of the resulting image")
 	flag.Parse()
 
+	// Parse the mix color
+	colHex := strings.TrimLeft(*col, "#") // Strip the #
+	fmt.Println(colHex)
+	colRed, _ := strconv.ParseUint(colHex[0:2], 16, 32)
+	colGreen, _ := strconv.ParseUint(colHex[2:4], 16, 32)
+	colBlue, _ := strconv.ParseUint(colHex[4:6], 16, 32)
+	mix := color.RGBA{uint8(colRed), uint8(colGreen), uint8(colBlue), 255}
+
+	// Generate random colors
 	light := tools.RandColor(mix)
 	dark := tools.RandColor(mix)
 
+	// Make an image
 	m := image.NewRGBA(image.Rect(0, 0, *w, *h))
 	draw.Draw(m, m.Bounds(), &image.Uniform{dark}, image.ZP, draw.Src) // Fill with a uniform color
 
